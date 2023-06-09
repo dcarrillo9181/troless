f54e98a393d3f149c62d8d700d3afa87fd738893
dcarrillo9181 <27480583+dcarrillo9181@users.noreply.github.com>
2023-06-09 12:44:59 -0700

    edited styling for top videos page

--- app/top_videos/top_video_comps/TopOptimisticVideos.tsx
+++ app/top_videos/top_video_comps/TopOptimisticVideos.tsx
@@ -15,12 +15,12 @@ interface Video {
 }
 
 const videos: Video[] = [
-  { id: 1, title: 'Video 1', description: 'Description', url: 'https://via.placeholder.com/150' },
-  { id: 2, title: 'Video 2', description: 'Description', url: 'https://via.placeholder.com/150' },
-  { id: 3, title: 'Video 3', description: 'Description', url: 'https://via.placeholder.com/150' },
-  { id: 4, title: 'Video 4', description: 'Description', url: 'https://via.placeholder.com/150' },
-  { id: 5, title: 'Video 5', description: 'Description', url: 'https://via.placeholder.com/150' },
-  { id: 6, title: 'Video 6', description: 'Description', url: 'https://via.placeholder.com/150' },
+  { id: 1, title: 'Title 1', description: 'Description', url: 'https://via.placeholder.com/400x400?text=Video+1' },
+  { id: 2, title: 'Title 2', description: 'Description', url: 'https://via.placeholder.com/400x400?text=Video+2' },
+  { id: 3, title: 'Title 3', description: 'Description', url: 'https://via.placeholder.com/400x400?text=Video+3' },
+  { id: 4, title: 'Title 4', description: 'Description', url: 'https://via.placeholder.com/400x400?text=Video+4' },
+  { id: 5, title: 'Title 5', description: 'Description', url: 'https://via.placeholder.com/400x400?text=Video+5' },
+  { id: 6, title: 'Title 6', description: 'Description', url: 'https://via.placeholder.com/400x400?text=Video+6' },
   // Add more video placeholders as needed
 ];
 
@@ -31,25 +31,24 @@ const TopOptimisticVideos: React.FC = () => {
       <Swiper
         navigation
         pagination={{ clickable: true }}
-        spaceBetween={16}
-        slidesPerView={3}
-        breakpoints={{
-          640: { slidesPerView: 2 },
-          1024: { slidesPerView: 3 },
-        }}
+        className="mySwiper"
       >
         {videos.map((video) => (
           <SwiperSlide key={video.id}>
-            <div className="bg-blue-100 rounded-md shadow p-4">
-              <img
-                src={video.url}
-                alt={video.title}
-                className="rounded-md h-48 object-cover mb-2"
-              />
-              <div className='flex'>
-                <h5 className="text-xl font-semibold text-gray-500">{video.description}</h5>
+            <div className="relative h-0" style={{ paddingBottom: '100%' }}>
+              <div className="absolute inset-0 flex items-center justify-center">
+                <div className="relative w-3/4 h-3/4 rounded-full overflow-hidden">
+                  <img src={video.url} alt={video.title} className="absolute inset-0 w-full h-full object-cover" />
+                  <div className="absolute inset-0 bg-black opacity-50"></div>
+                  <div className="absolute inset-0 flex items-center justify-center">
+                    <div className="w-1/2 h-1/2 rounded-full bg-white"></div>
+                  </div>
+                </div>
               </div>
-              <h3 className="text-lg font-semibold">{video.title}</h3>
+            </div>
+            <div className="p-4">
+              <h2 className="text-lg font-bold mb-2">{video.title}</h2>
+              <p className="text-gray-700">{video.description}</p>
             </div>
           </SwiperSlide>
         ))}
